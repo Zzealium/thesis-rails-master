b7d13f27742a910b6ef00782dc644ef56acce05a
Zzealium <kaleb.walker@tensility.com>
2022-06-06 15:56:04 -0700

    a

--- lib/thesis/models/page.rb
+++ lib/thesis/models/page.rb
@@ -10,8 +10,9 @@ module Thesis
 
     after_save :update_subpage_slugs
 
-    validates uniqueness: { message: "There's already a page at that location." },
-      presence: true
+    validates :slug
+      # uniqueness: { message: "There's already a page at that location." },
+      # presence: true,
       # allow_blank: false,
       # allow_null: false
 
