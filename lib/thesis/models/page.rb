31a6f3c7a95206888c241ac34a5d20bfcfca8f20
Zzealium <kaleb.walker@tensility.com>
2022-06-06 16:00:08 -0700

    ad

--- lib/thesis/models/page.rb
+++ lib/thesis/models/page.rb
@@ -13,8 +13,8 @@ module Thesis
     validates :slug,
       uniqueness: { message: "There's already a page at that location." },
       presence: true,
-      allow_blank: false
-      # allow_null: false
+      allow_blank: false,
+      allow_null: false
 
     def update_slug
       self.slug = "/" << self.name.to_s.parameterize
