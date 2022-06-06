08919cf824442ae0d7d95d816319a4841ca3529c
Zzealium <kaleb.walker@tensility.com>
2022-06-06 09:43:56 -0700

    awd

--- lib/thesis/controllers/thesis_controller.rb
+++ lib/thesis/controllers/thesis_controller.rb
@@ -1,5 +1,5 @@
 module Thesis
-  class ThesisController < ApplicationController::Base64
+  class ThesisController < ApplicationController#::Base64
     include Thesis::ControllerHelpers
 
     def show
