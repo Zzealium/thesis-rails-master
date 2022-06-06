3997904914e26735555efc3244542a9a6db7ffc9
Zzealium <kaleb.walker@tensility.com>
2022-06-06 16:07:15 -0700

    asd

--- lib/thesis/models/page.rb
+++ lib/thesis/models/page.rb
@@ -14,7 +14,7 @@ module Thesis
       uniqueness: { message: "There's already a page at that location." },
       presence: true,
       allow_blank: false
-      # allow_null: false
+      # allow_null: false #This needs to be fixed at some point. 
 
     def update_slug
       self.slug = "/" << self.name.to_s.parameterize
