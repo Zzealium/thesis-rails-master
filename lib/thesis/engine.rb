2daac625532d2bcca757f90804e21ed04e390787
Zzealium <kaleb.walker@tensility.com>
2022-10-11 16:47:04 -0700

    a

--- lib/thesis/engine.rb
+++ lib/thesis/engine.rb
@@ -4,10 +4,10 @@ module Thesis
     initializer 'thesis.action_controller' do |app|
       require "thesis/controllers/controller_helpers"
       require "thesis/controllers/thesis_controller"
-      
+
       ActiveSupport.on_load(:action_controller) do
         include ::Thesis::ControllerHelpers
-        helper_method :current_page, :root_pages, :page_is_editable?, :thesis_editor
+        helper_method :current_page, :root_pages, :thesis_editor
       end
     end
   end
