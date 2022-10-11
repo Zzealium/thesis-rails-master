bd56d5ea50315ca6efedfe972b988d9157d26f9a
Zzealium <kaleb.walker@tensility.com>
2022-10-11 12:45:41 -0700

    awdawd

--- lib/thesis.rb
+++ lib/thesis.rb
@@ -1,6 +1,6 @@
 require "thesis/version"
 require "thesis/exceptions"
-require "app/controllers/controller_helpers"
+require "thesis/controllers/controller_helpers"
 require "thesis/routing/routes"
 require "thesis/routing/route_constraint"
 require "thesis/models/page"
